9731083196930474b3c6cb7070041c33afb30966
AoiHosizora <a970335605@hotmail.com>
2020-07-31 15:50:58 +0800

    feat(route): move route to dev.route branch

--- xfiber/xfiber_test.go
+++ xfiber/xfiber_test.go
@@ -6,36 +6,6 @@ import (
 	"testing"
 )
 
-func TestComposite(t *testing.T) {
-	app := fiber.New()
-	app.Get("/:id", Composite("id",
-		M(func(c *fiber.Ctx) {
-			log.Println(11, c.Params("id"))
-		}, func(c *fiber.Ctx) {
-			log.Println(12, c.Params("id"))
-		}),
-		P("a", func(c *fiber.Ctx) {
-			log.Println(21, c.Params("id"))
-		}, func(c *fiber.Ctx) {
-			log.Println(22, c.Params("id"))
-			c.Next()
-		}, func(c *fiber.Ctx) {
-			log.Println(23, c.Params("id"))
-		}),
-		P("b", func(c *fiber.Ctx) {
-			log.Println(31, c.Params("id"))
-		}, func(c *fiber.Ctx) {
-			log.Println(32, c.Params("id"))
-		}),
-		N(func(c *fiber.Ctx) {
-			log.Println(41, c.Params("id"))
-		}),
-	), func(c *fiber.Ctx) {
-		log.Println(51, c.Params("id"))
-	})
-	_ = app.Listen("1234")
-}
-
 func TestDumpRequest(t *testing.T) {
 	app := fiber.New()
 	app.Get("a", func(c *fiber.Ctx) {
